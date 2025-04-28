55a50e372097270f81ee9e2dcbd3f8b65a317729
amhunpdx <amhun@pdx.edu>
2025-04-28 10:45:25 -0700

    switch to merged install

--- build/air_cleaning_pkg/prefix_override/sitecustomize.py
+++ build/air_cleaning_pkg/prefix_override/sitecustomize.py
@@ -1,4 +1,4 @@
 import sys
 if sys.prefix == '/usr':
     sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/lac/workspace/lac/install/air_cleaning_pkg'
+    sys.prefix = sys.exec_prefix = '/home/lac/workspace/lac/install'
